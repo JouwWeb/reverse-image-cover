3687b24eb50404cfbf046c0a8c939149e85544eb
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2015-08-11 10:17:26 +0200

    Extend demo set-up.

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -8,7 +8,7 @@ return [
   ['fitsExactlySmaller', [500, 200, 0.5, 0.5], [250, 100], [0, 0, 250, 100]],
   ['translationOnlyX', [200, 200, 1, 0], [500, 200], [300, 0, 200, 200]],
   ['translationOnlyXOtherEdge', [200, 200, 0, 0], [500, 200], [0, 0, 200, 200]],
-  ['translationYOnly', [100, 100, 0.5, 0], [100, 400], [0, 150, 100, 100]],
-  ['translationYOnlyVerySmallImage', [100, 100, 0.5, 0], [4, 16], [0, 6, 4, 4]],
+  ['translationYOnly', [100, 100, 0.5, 0.5], [100, 400], [0, 150, 100, 100]],
+  ['translationYOnlyVerySmallImage', [100, 100, 0.5, 0], [4, 16], [0, 0, 4, 4]],
   ['translationNearBorder', [200, 200, 0.9, 0.5], [600, 400], [180, 0, 400, 400]],
 ];
--- tests/demo.php
+++ tests/demo.php
@@ -29,7 +29,7 @@
 
     <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&bust=<?=time()?>); background-size: cover; background-position: <?=$fixture[1][2] * 100?>% <?=$fixture[1][3] * 100?>%"><span style="background-color: white;">orig</span></div>
     <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $crop)?>&bust=<?=time()?>); background-size: cover; background-position: 50% 50%"><span style="background-color: white;">crop</span></div>
-    <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $fixture[3])?>&bust=<?=time()?>); background-size: cover; background-position: 50% 50%"><span style="background-color: white;">fixture</span></div>
+    <div style="opacity: 0.6; margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $fixture[3])?>&bust=<?=time()?>); background-size: cover; background-position: 50% 50%"><span style="background-color: white;">fixture</span></div>
 
   </div>
 
