8af9ffce0372cd8ab9807b19e850ffc8246dc1e5
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2015-08-11 09:58:55 +0200

    Improve demo set up.

--- tests/demo.php
+++ tests/demo.php
@@ -9,6 +9,8 @@
 
 <?php foreach ($fixtures as $fixture):?>
 
+  <h2 style="clear: both"><?=$fixture[0]?></h2>
+
   <?php
 
   $viewport = new \ImageCover\BackgroundCoverViewport(
@@ -22,7 +24,7 @@
 
   ?>
 
-  <div style="position: relative; clear: both;">
+  <div style="position: relative;">
 
     <div style="margin: 10px; float: left; border: 1px solid black; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>); background-size: cover; background-position: <?=$fixture[1][2] * 100?>% <?=$fixture[1][3] * 100?>%"></div>
     <div style="margin: 10px; float: left; border: 1px solid black; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $crop)?>); background-size: cover; background-position: 50% 50%"></div>
--- tests/image.php
+++ tests/image.php
@@ -1,9 +1,40 @@
 <?php
 
+/*
+ * http://geekthis.net/blog/87/php-gradient-images-rectangle-gd
+ * bool image_gradientrect(resource $image, int $x1, int $y1, int $x2, int $y2, string $HexColorCodeStart, string $HexColorCodeEnd);
+ */
+function image_gradientrect($img,$x,$y,$x1,$y1,$start,$end) {
+   if($x > $x1 || $y > $y1) {
+      return false;
+   }
+   $s = array(
+      hexdec(substr($start,0,2)),
+      hexdec(substr($start,2,2)),
+      hexdec(substr($start,4,2))
+   );
+   $e = array(
+      hexdec(substr($end,0,2)),
+      hexdec(substr($end,2,2)),
+      hexdec(substr($end,4,2))
+   );
+   $steps = $y1 - $y;
+   for($i = 0; $i < $steps; $i++) {
+      $r = $s[0] - ((($s[0]-$e[0])/$steps)*$i);
+      $g = $s[1] - ((($s[1]-$e[1])/$steps)*$i);
+      $b = $s[2] - ((($s[2]-$e[2])/$steps)*$i);
+      $color = imagecolorallocate($img,$r,$g,$b);
+      imagefilledrectangle($img,$x,$y+$i,$x1,$y+$i+1,$color);
+   }
+   return true;
+}
+
 $im = imagecreate($_GET['width'], $_GET['height']);
 
-$orange = imagecolorallocate($im, 220, 210, 60);
+$white = imagecolorallocate($im, 255, 255, 255);
 $black = imagecolorallocate($im, 0, 0, 0);
+
+image_gradientrect($im, 0,0, $_GET['width'], $_GET['height'], 'FF0000', '0000FF');
 
 for ($i = 0; $i <= $_GET['width']; $i += 100) {
   imageline($im, $i - 1, 0, $i - 1, $_GET['height'], $black);
