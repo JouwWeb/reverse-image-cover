3e8e392483b91a520db2cd82c072064c19ec7de4
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2015-08-11 10:13:09 +0200

    Improve demo page.

--- tests/Fixture.php
+++ tests/Fixture.php
@@ -9,5 +9,6 @@ return [
   ['translationOnlyX', [200, 200, 1, 0], [500, 200], [300, 0, 200, 200]],
   ['translationOnlyXOtherEdge', [200, 200, 0, 0], [500, 200], [0, 0, 200, 200]],
   ['translationYOnly', [100, 100, 0.5, 0], [100, 400], [0, 150, 100, 100]],
-  ['translationYOnlyVerySmallImage', [100, 100, 0.5, 0], [4, 16], [0, 6, 4, 4]]
+  ['translationYOnlyVerySmallImage', [100, 100, 0.5, 0], [4, 16], [0, 6, 4, 4]],
+  ['translationNearBorder', [200, 200, 0.9, 0.5], [600, 400], [180, 0, 400, 400]],
 ];
--- tests/demo.php
+++ tests/demo.php
@@ -6,6 +6,7 @@
   $fixtures = require 'Fixture.php';
 ?>
 
+<body style="width: 3000px">
 
 <?php foreach ($fixtures as $fixture):?>
 
@@ -26,9 +27,12 @@
 
   <div style="position: relative;">
 
-    <div style="margin: 10px; float: left; border: 1px solid black; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>); background-size: cover; background-position: <?=$fixture[1][2] * 100?>% <?=$fixture[1][3] * 100?>%"></div>
-    <div style="margin: 10px; float: left; border: 1px solid black; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $crop)?>); background-size: cover; background-position: 50% 50%"></div>
+    <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&bust=<?=time()?>); background-size: cover; background-position: <?=$fixture[1][2] * 100?>% <?=$fixture[1][3] * 100?>%"><span style="background-color: white;">orig</span></div>
+    <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $crop)?>&bust=<?=time()?>); background-size: cover; background-position: 50% 50%"><span style="background-color: white;">crop</span></div>
+    <div style="margin: 10px; float: left; width: <?=$fixture[1][0]?>px; height: <?=$fixture[1][1]?>px; background-image: url(image.php?width=<?=$fixture[2][0]?>&height=<?=$fixture[2][1]?>&crop=<?=implode(',', $fixture[3])?>&bust=<?=time()?>); background-size: cover; background-position: 50% 50%"><span style="background-color: white;">fixture</span></div>
 
   </div>
 
 <?php endforeach?>
+
+</body>
